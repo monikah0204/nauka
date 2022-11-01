80ee2335158253d0b15903bf29be86058ce5df8e
monikah0204 <moniaharazin@gmail.com>
2022-11-01 14:32:23 +0100

    dodanie funkcji do liczenia silni

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dzien dobry!");
-        System.out.println("napis");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj liczbę do obliczenia silni: ");
+        int n = scanner.nextInt();
+        System.out.println("Silnia z " + n       + ": " + calculateSilnia(n));
+        System.out.println("Silnia z " + (n + 1) + ": " + calculateSilnia(n + 1));
+        System.out.println("Silnia z " + (n + 2) + ": " + calculateSilnia(n + 2));
     }
+
+    private static int calculateSilnia(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
 }
